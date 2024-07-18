972d7b27714762c037274b3233c40989ceef8a64
Oshri <oshrimeg@gmail.com>
2024-07-18 18:14:29 +0300

    datetime1

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from datetime import datetime
 
-today = datetime.now()
-print(today)
+# Get current date and time
+current_datetime = datetime.now()
+
+# Format the current datetime as desired
+formatted_datetime = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
+
+# Print the formatted datetime
+print("Current Date and Time:", formatted_datetime)
